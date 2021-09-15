31368edb46cbb929c5cdfd7ad49a0d9dcfe119e3
LewisForYou <74797340+LewisForYou@users.noreply.github.com>
2021-09-15 12:18:28 +0300

    addmouselistner v1

--- src/Components/MainPage/MainPage.jsx
+++ src/Components/MainPage/MainPage.jsx
@@ -1,10 +1,20 @@
+import React from 'react'
 import './MainPage.css'
 import Logo from './index.png'
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import { faClipboard } from '@fortawesome/free-solid-svg-icons'
 import { faGithub, faTelegram} from '@fortawesome/free-brands-svg-icons'
 
-function MainPage() {
+class MainPage extends React.Component {
+  state = {
+    showGithub: false,
+    showTelegram: false,
+    showProjects: false,
+    showBox: false
+  }
+  handleBoxToggle = () => this.setState({ showBox: true });
+  handleBoxToggleV2 = () => this.setState({ showBox: false });
+  render(){
     return (
       <div className="MainBlock">
           <div className="Logo">
@@ -21,10 +31,18 @@ function MainPage() {
           </div>
           <div className="Notify">
             <span className="NotifyGithub">github.com/lewisforyou</span>
+            <span className="NotifyTelegram">t.me/lewisdev</span>
+            <span className="NotifyProjects">projects</span>
+          </div>
+          <div onMouseEnter={this.handleBoxToggle}
+          onMouseOut ={this.handleBoxToggleV2}
+          className={`container${this.state.showBox ? " show" : ""}`}
+          >активировать
           </div>
       </div>
     );
   }
+}
   
   export default MainPage
   
